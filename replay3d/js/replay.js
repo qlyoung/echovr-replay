b371d29ef11b396d9c983a5a6f6ac70f1429b661
Quentin Young <qlyoung@qlyoung.net>
2018-12-11 18:48:55 -0500

    js: defer player creation until mesh loads finish
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- replay3d/js/replay.js
+++ replay3d/js/replay.js
@@ -1,5 +1,6 @@
 var scene, camera, renderer, mesh;
 var ambient, light;
+var savedgame, frames;
 
 var loadingScreen = {
     scene: new THREE.Scene(),
@@ -10,25 +11,18 @@ var loadingScreen = {
     )
 };
 var loadingManager = null;
-var RESOURCES_LOADED = false;
 
 // Models index
 var models = {
     arena: {
         obj:"models/map.obj",
         mesh: null
-    }
-};
-
-var echoModel = {
-    echo: {
+    },
+    echoOrange: {
         obj:"models/echo.obj",
         mesh: null
-    }
-};
-
-var blueModel = {
-    echo: {
+    },
+    echoBlue: {
         obj:"models/echo.obj",
         mesh: null
     }
@@ -37,6 +31,8 @@ var blueModel = {
 // Meshes index
 var meshes = {};
 
+orangePlayers = []
+bluePlayers = []
 
 function init(){
     //Create Scene
@@ -70,100 +66,100 @@ function init(){
     loadingManager.onProgress = function(item, loaded, total){
        //console.log(item, loaded, total);
     };
-    loadingManager.onLoad = function(){
-      //console.log("loaded all resources");
-        RESOURCES_LOADED = true;
-        onResourcesLoaded();
-    };
+    loadingManager.onLoad = onResourcesLoaded;
 
-    // Load Arena Instance
+    // Start loading meshes, and when complete, call onResourcesLoaded
     var objLoader = new THREE.OBJLoader(loadingManager);
     for( var _key in models ){
         (function(key){
             objLoader.load(models[key].obj, function(mesh){
                 mesh.traverse(function(node){
                     if( node instanceof THREE.Mesh ){
-                        node.material = new THREE.MeshLambertMaterial({color: 0x00ff00, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        node.scale.set(.12,.12,.12);
+			console.log(key);
+			if (key == "arena") {
+				console.log("loading arena");
+                        	node.material = new THREE.MeshLambertMaterial({color: 0x00ff00, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
+                        	node.scale.set(.12,.12,.12);
+			}
+			else if (key == "echoBlue") {
+                        	node.material = new THREE.MeshLambertMaterial({color: 0x5333ff, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
+                        	node.scale.set(.1,.1,.1);
+			}
+			else if (key == "echoOrange") {
+                        	node.material = new THREE.MeshLambertMaterial({color: 0xf46036, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
+                        	node.scale.set(.1,.1,.1);
+			}
                     }
                 });
                 models[key].mesh = mesh;
+		console.log("models[" + key + "].mesh is ");
+		console.log(models[key].mesh);
             });
         })(_key);
     }
 
-    // Load Orange Team Instance
-    for( var echoKey in echoModel ){
-        (function(key){
-            objLoader.load(echoModel[key].obj, function(mesh){
-                mesh.traverse(function(node){
-                    if( node instanceof THREE.Mesh ){
-                        node.material = new THREE.MeshLambertMaterial({color: 0xf46036, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        node.scale.set(.1,.1,.1);
-                    }
-                });
-                echoModel[key].mesh = mesh;
-            });
-        })(echoKey);
-    }
-
-    // Load Blue Team Instance
-    for( var blue in blueModel ){
-        (function(key){
-            objLoader.load(blueModel[key].obj, function(mesh){
-                mesh.traverse(function(node){
-                    if( node instanceof THREE.Mesh ){
-                        node.material = new THREE.MeshLambertMaterial({color: 0x5333ff, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        node.scale.set(.1,.1,.1);
-                    }
-                });
-                blueModel[key].mesh = mesh;
-            });
-        })(blue);
-    }
-
-    // Load Frames
+    // this code runs in parallel to the loading code above; while we wait, lets load the rest of our resources
     var request = new XMLHttpRequest();
     request.open("GET", "../data/game.json", false);
     request.send(null)
-    var savedGame = JSON.parse(request.responseText);
-    var frames = savedGame['frames'];
+    savedGame = JSON.parse(request.responseText);
+    frames = savedGame['frames'];
+}
 
-    orangePlayers = []
-    bluePlayers = []
-    meshes["echo0"] = echoModel.echo.mesh.clone();
-    meshes["echo1"] = echoModel.echo.mesh.clone();
-    meshes["echo2"] = echoModel.echo.mesh.clone();
-    meshes["echo3"] = blueModel.echo.mesh.clone();
-    meshes["echo4"] = blueModel.echo.mesh.clone();
-    meshes["echo5"] = blueModel.echo.mesh.clone();
-    var playerGeo = new THREE.SphereGeometry( 1, 32, 32 );
-    var playerOrangeMat = new THREE.MeshBasicMaterial({color: 0xffa500});
-    var playerBlueMat = new THREE.MeshBasicMaterial({color: 0x0000ff});
+function setPlayerPosition(player, framepos) {
+    player.position.x = framepos[2];
+    player.position.y = framepos[0];
+    player.position.z = framepos[1];
+}
 
+// Runs when all resources are loaded
+function onResourcesLoaded(){
+    RESOURCES_LOADED = true;
+
+    // now that our resources have loaded, lets initialize the meshes dictionary...
+    meshes["echo0"] = models.echoBlue.mesh.clone();
+    meshes["echo1"] = models.echoBlue.mesh.clone();
+    meshes["echo2"] = models.echoBlue.mesh.clone();
+    meshes["echo3"] = models.echoOrange.mesh.clone();
+    meshes["echo4"] = models.echoOrange.mesh.clone();
+    meshes["echo5"] = models.echoOrange.mesh.clone();
+    meshes["arena"] = models.arena.mesh.clone();
+
+    console.log("meshes[echo0] is");
+    console.log(meshes["echo0"]);
+    console.log("meshes[echo3] is");
+    console.log(meshes["echo3"]);
+
+    // ..create and add the players...
     orangePlayers.push(meshes["echo0"]);
     orangePlayers.push(meshes["echo1"]);
     orangePlayers.push(meshes["echo2"]);
     bluePlayers.push(meshes["echo3"]);
     bluePlayers.push(meshes["echo4"]);
     bluePlayers.push(meshes["echo5"]);
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 3; i++) {
         scene.add(orangePlayers[i]);
         scene.add(bluePlayers[i]);
     }
 
-    var setPlayerPosition = function(player, framepos) {
-        player.position.x = framepos[2];
-        player.position.y = framepos[0];
-        player.position.z = framepos[1];
-    }
+    // ...reposition and add the arena...
+    meshes["arena"].position.set(0, 0, 0);
+    scene.add(meshes["arena"]);
+
+    // and finally start rendering
+    run();
+}
+
+function run(){
+
     /* frame index */
     var idx = 0;
 
-    var animate = function () {
+    var anim = function () {
+        requestAnimationFrame( anim );
+
         frame = frames[idx];
         idx += 1;
-        requestAnimationFrame( animate );
 
         for (var i = 0; i < 3; i++) {
             setPlayerPosition(orangePlayers[i], frame.teams[0].players[i].position);
@@ -174,40 +170,8 @@ function init(){
     };
 
 
-    animate();
-
-}
-
-// Runs when all resources are loaded
-function onResourcesLoaded(){
-
-    // Clone models into meshes.
-    meshes["arena"] = models.arena.mesh.clone();
-    meshes["echo"] = echoModel.echo.mesh.clone();
-
-    // Reposition individual meshes, then add meshes to scene
-    meshes["arena"].position.set(0, 0, 0);
-    scene.add(meshes["arena"]);
-
-    meshes["echo"].position.set(0, 5, 0);
-}
-
-function animate(){
-
-    // Play the loading screen until resources are loaded.
-    if( RESOURCES_LOADED == false ){
-        requestAnimationFrame(animate);
-
-        loadingScreen.box.position.x -= 0.05;
-        if( loadingScreen.box.position.x < -10 ) loadingScreen.box.position.x = 10;
-        loadingScreen.box.position.y = Math.sin(loadingScreen.box.position.x);
-
-        renderer.render(loadingScreen.scene, loadingScreen.camera);
-        return;
-    }
-
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    anim();
+    return;
 }
 
 window.onload = init;
