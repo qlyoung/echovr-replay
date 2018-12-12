04669018791e69668edeb32c0745cc336cda23c3
Quentin Young <qlyoung@qlyoung.net>
2018-12-12 00:07:07 -0500

    js: reformat replay.js
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- replay3d/js/replay.js
+++ replay3d/js/replay.js
@@ -1,14 +1,14 @@
 var models = {
     arena: {
-        obj:"models/map.obj",
+        obj: "models/map.obj",
         mesh: null
     },
     echoOrange: {
-        obj:"models/echo.obj",
+        obj: "models/echo.obj",
         mesh: null
     },
     echoBlue: {
-        obj:"models/echo.obj",
+        obj: "models/echo.obj",
         mesh: null
     }
 };
@@ -23,19 +23,19 @@ var meshes = {};
 orangePlayers = [];
 bluePlayers = [];
 
-function init(){
+function init() {
     //Create Scene
     scene = new THREE.Scene();
 
     //Create Camera
-    camera = new THREE.PerspectiveCamera( 20, window.innerWidth/window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.set(80, 25, 80);
-    camera.lookAt(0,-10,0);
+    camera.lookAt(0, -10, 0);
 
     //Create Renderer
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     renderer.sortObjects = false;
 
     //Create Lighting
@@ -43,34 +43,50 @@ function init(){
     scene.add(ambient);
     light = new THREE.PointLight(0xffffff, 1, 10000);
     light.position.set(0, -35, 30);
-    scene.add( light );
+    scene.add(light);
 
     //Object Loading Manager
     var loadingManager = new THREE.LoadingManager();
-    loadingManager.onProgress = function(item, loaded, total){
-       //console.log(item, loaded, total);
+    loadingManager.onProgress = function(item, loaded, total) {
+        //console.log(item, loaded, total);
     };
     loadingManager.onLoad = onResourcesLoaded;
 
     // Start loading meshes, and when complete, call onResourcesLoaded
     var objLoader = new THREE.OBJLoader(loadingManager);
-    for( var _key in models ){
-        (function(key){
-            objLoader.load(models[key].obj, function(mesh){
-                mesh.traverse(function(node){
-                    if( node instanceof THREE.Mesh ){
-			if (key == "arena") {
-                        	node.material = new THREE.MeshLambertMaterial({color: 0x00ff00, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        	node.scale.set(.12,.12,.12);
-			}
-			else if (key == "echoBlue") {
-                        	node.material = new THREE.MeshLambertMaterial({color: 0x5333ff, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        	node.scale.set(.1,.1,.1);
-			}
-			else if (key == "echoOrange") {
-                        	node.material = new THREE.MeshLambertMaterial({color: 0xf46036, transparent: true, opacity: 0.1, wireframe: true, wireframeLinewidth: 0.1});
-                        	node.scale.set(.1,.1,.1);
-			}
+    for (var _key in models) {
+        (function(key) {
+            objLoader.load(models[key].obj, function(mesh) {
+                mesh.traverse(function(node) {
+                    if (node instanceof THREE.Mesh) {
+                        if (key == "arena") {
+                            node.material = new THREE.MeshLambertMaterial({
+                                color: 0x00ff00,
+                                transparent: true,
+                                opacity: 0.1,
+                                wireframe: true,
+                                wireframeLinewidth: 0.1
+                            });
+                            node.scale.set(.12, .12, .12);
+                        } else if (key == "echoBlue") {
+                            node.material = new THREE.MeshLambertMaterial({
+                                color: 0x5333ff,
+                                transparent: true,
+                                opacity: 0.1,
+                                wireframe: true,
+                                wireframeLinewidth: 0.1
+                            });
+                            node.scale.set(.1, .1, .1);
+                        } else if (key == "echoOrange") {
+                            node.material = new THREE.MeshLambertMaterial({
+                                color: 0xf46036,
+                                transparent: true,
+                                opacity: 0.1,
+                                wireframe: true,
+                                wireframeLinewidth: 0.1
+                            });
+                            node.scale.set(.1, .1, .1);
+                        }
                     }
                 });
                 models[key].mesh = mesh;
@@ -88,19 +104,21 @@ function init(){
     /* Show a loading screen */
     var loadingScreen = {
         scene: new THREE.Scene(),
-        camera: new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 100),
+        camera: new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100),
         box: new THREE.Mesh(
-            new THREE.BoxGeometry(0.5,0.5,0.5),
-            new THREE.MeshBasicMaterial({ color:0x4444ff })
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
+            new THREE.MeshBasicMaterial({
+                color: 0x4444ff
+            })
         )
     };
 
-    loadingScreen.box.position.set(0,0,5);
+    loadingScreen.box.position.set(0, 0, 5);
     loadingScreen.camera.lookAt(loadingScreen.box.position);
     loadingScreen.scene.add(loadingScreen.box);
 }
 
-function onResourcesLoaded(){
+function onResourcesLoaded() {
     meshes["echo0"] = models.echoBlue.mesh.clone();
     meshes["echo1"] = models.echoBlue.mesh.clone();
     meshes["echo2"] = models.echoBlue.mesh.clone();
@@ -127,19 +145,19 @@ function onResourcesLoaded(){
     run();
 }
 
-function run(){
+function run() {
 
-    var setPlayerPosition = function (player, framepos) {
-    	player.position.x = framepos[2];
-    	player.position.y = framepos[0];
-    	player.position.z = framepos[1];
+    var setPlayerPosition = function(player, framepos) {
+        player.position.x = framepos[2];
+        player.position.y = framepos[0];
+        player.position.z = framepos[1];
     }
 
     /* frame index */
     var idx = 0;
 
-    var anim = function () {
-        requestAnimationFrame( anim );
+    var anim = function() {
+        requestAnimationFrame(anim);
 
         frame = savedgame.frames[idx];
         idx += 1;
@@ -149,7 +167,7 @@ function run(){
             setPlayerPosition(bluePlayers[i], frame.teams[1].players[i].position);
         }
 
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
     };
 
 
@@ -157,4 +175,3 @@ function run(){
 }
 
 window.onload = init;
-
