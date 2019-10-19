cacd0302384ffa1d3c7ea2af708929a7a0f5a929
Quentin Young <qlyoung@qlyoung.net>
2019-10-19 02:32:52 -0400

    record.py: use asyncio
    Rewrite recorder to use asyncio / aiohttp. This allows us to run at an
    effecitvely unlimited capture rate.
    
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- record.py
+++ record.py
@@ -9,6 +9,9 @@ import sys
 import argparse
 import pprint
 
+import aiohttp
+import asyncio
+
 
 def save_game(frames, caprate):
     """
@@ -48,92 +51,103 @@ def save_game(frames, caprate):
 
     return fname
 
-
-def capture(caprate, session=None):
-    """
-    Capture game.
-
-    :param session: requests session to use
-    :param caprate: fps to capture
-    """
-    caprate = float(caprate)
-    session = session if session is not None else requests.session()
-
+class RecordState(object):
     frames = []
     saved = True
     laststate = "none"
     state = "none"
     framecount = 0
     clock0 = 0
-    actual_caprate = caprate
     lastclock = 0
     connected = False
 
     recordstates = ["playing", "score", "round_start"]
 
+    def __init__(self, caprate):
+        self.caprate = caprate
+        self.actual_caprate = self.caprate;
+
+    
+async def rx_frame(session, state):
+    try:
+        resp = await session.get("http://127.0.0.1/session")
+    except ConnectionError:
+        print("Couldn't connect to Echo client")
+        exit()
+
+    try:
+        frame = await resp.json()
+        state.laststate = state.state
+        state.state = frame["game_status"]
+    except:
+        exit()
+
+    if state.state != state.laststate:
+        print("\n{} -> {}".format(state.laststate, state.state))
+        state.lastclock = time.time()
+
+    if not state.connected:
+        state.connected = True
+        print("Connected to Echo VR")
+
+    if state.state == "post_match":
+        if not state.saved:
+            if len(state.frames) > state.caprate * 5:
+                print("\nSaving game...")
+                sname = save_game(state.frames, state.actual_caprate)
+                print("\nSaved {} frames to {}\n".format(len(state.frames), sname))
+            else:
+                print("\nSkipping save, less than 5 seconds of data")
+            state.frames = []
+            state.framecount = 0
+            state.saved = True
+            state.actual_caprate = state.caprate
+            state.clock0 = 0
+    elif state.state in RecordState.recordstates:
+        if state.saved:
+            state.saved = False
+            state.clock0 = time.time()
+            state.lastclock = time.time()
+
+        # the most ghetto clock on the face of the earth
+        state.elapsed = time.time() - state.lastclock
+        if state.elapsed >= 1.0 / caprate - 0.001:
+            state.lastclock = time.time()
+            state.framecount += 1
+            state.actual_caprate = float(state.framecount) / (time.time() - state.clock0)
+            state.frames.append(frame)
+            if (state.framecount % 3) == 0:
+                sys.stdout.write(
+                    "\rCaptured frame {} ({:.2f} fps)".format(
+                        state.framecount, state.actual_caprate
+                    )
+                )
+                sys.stdout.flush()
+
+
+async def capture(state, session=None):
+    """
+    Capture game.
+
+    :param session: requests session to use
+    :param caprate: fps to capture
+    """
+    session = session if session is not None else aiohttp.ClientSession()
+
     try:
         while True:
             try:
-                result = session.get("http://127.0.0.1/session")
-                _ = result.content
+                await asyncio.gather(*[asyncio.create_task(rx_frame(session, state)) for i in range(1, 5)])
             except ConnectionError:
                 print("Couldn't connect to Echo client")
                 exit()
 
-            try:
-                frame = json.loads(result.text)
-                laststate = state
-                state = frame["game_status"]
-            except:
-                continue
+    except KeyboardInterrupt:
+        print("\nCaught keyboard interrupt")
 
-            if not connected:
-                connected = True
-                print("Connected to Echo VR")
-
-            if state == "post_match":
-                if not saved:
-                    if len(frames) > caprate * 5:
-                        print("\nSaving game...")
-                        sname = save_game(frames, actual_caprate)
-                        print("\nSaved {} frames to {}\n".format(len(frames), sname))
-                    else:
-                        print("\nSkipping save, less than 5 seconds of data")
-                    frames = []
-                    framecount = 0
-                    saved = True
-                    actual_caprate = caprate
-                    clock0 = 0
-            elif state in recordstates:
-                if saved:
-                    saved = False
-                    clock0 = time.time()
-                    lastclock = time.time()
-
-                # the most ghetto clock on the face of the earth
-                elapsed = time.time() - lastclock
-                if elapsed >= 1.0 / caprate - 0.001:
-                    lastclock = time.time()
-                    framecount += 1
-                    actual_caprate = float(framecount) / (time.time() - clock0)
-                    frames.append(frame)
-                    if (framecount % 3) == 0:
-                        sys.stdout.write(
-                            "\rCaptured frame {} ({:.2f} fps)".format(
-                                framecount, actual_caprate
-                            )
-                        )
-                        sys.stdout.flush()
-
-            if state != laststate:
-                print("\n{} -> {}".format(laststate, state))
-
-    except:
-        print("\nCaught exception")
-
-    if len(frames) > 0:
-        sname = save_game(frames, actual_caprate)
-        print("\nSaved {} frames to {}\n".format(len(frames), sname))
+    if len(state.frames) > 0:
+        sname = save_game(state.frames, state.actual_caprate)
+        print("\nSaved {} frames to {}\n".format(len(state.frames), sname))
 
 
 CAPRATE_HZ = 60.0
@@ -150,4 +164,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     caprate = args.framerate
-    capture(caprate)
+    print("Requested caprate: {}".format(caprate))
+    asyncio.run(capture(RecordState(caprate)))
